240cf74223c64798fdcd5ee8cf27b4e259e1e780
tanvirahmed725 <91294146+tanvirahmed725@users.noreply.github.com>
2022-04-17 04:23:41 -0400

    Respawn/Fade Effect

--- Rezet/Assets/Scripts/RespawnScript.cs
+++ Rezet/Assets/Scripts/RespawnScript.cs
@@ -1,45 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RespawnScript : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
-    //flashing for death
-    
+    public Animator animator;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(("Player")))
-        {
-            player.transform.position = respawnPoint.transform.position;
-            Physics.SyncTransforms();
-        }
+        //Fade out
+        animator.SetBool("FadeOut", true);
 
-        Renderer[] meshIndex = player.GetComponentsInChildren<Renderer>();
 
-        StartCoroutine(Flash());
-        IEnumerator Flash()
+        StartCoroutine(waiter());
+        IEnumerator waiter()
         {
-            // This will wait 1 second like Invoke could do, remove this if you don't need it
             yield return new WaitForSeconds(0.5f);
 
 
-            float timePassed = 0;
-            float flashSpeed = 0.25f;
-            while (timePassed < 0.25f)
+            //Transport Player to respawn point
+            if (other.CompareTag(("Player")))
             {
-                foreach (Renderer r in meshIndex)
-                    r.enabled = !r.enabled;
-                timePassed += Time.deltaTime;
-
-                yield return new WaitForSeconds(flashSpeed -= Time.deltaTime);
+                player.transform.position = respawnPoint.transform.position;
+                Physics.SyncTransforms();
             }
+            //Fade In
 
-            foreach (Renderer r in meshIndex)
-                r.enabled = true;
+            animator.SetBool("FadeOut", false);
+
+            //Flash Death
+            Renderer[] meshIndex = player.GetComponentsInChildren<Renderer>();
+
+            StartCoroutine(Flash());
+            IEnumerator Flash()
+            {
+                // This will wait 1 second like Invoke could do, remove this if you don't need it
+                yield return new WaitForSeconds(0.5f);
+
+
+                float timePassed = 0;
+                float flashSpeed = 0.25f;
+                while (timePassed < 0.25f)
+                {
+                    foreach (Renderer r in meshIndex)
+                        r.enabled = !r.enabled;
+                    timePassed += Time.deltaTime;
+
+                    yield return new WaitForSeconds(flashSpeed -= Time.deltaTime);
+                }
+
+                foreach (Renderer r in meshIndex)
+                    r.enabled = true;
+            }
         }
+
+        
       
     }
 
